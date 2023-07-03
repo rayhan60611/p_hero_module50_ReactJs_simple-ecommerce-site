9616cd85d29dd9ec2faddcc515fc725daa7e26ef
rayhan60611 <pecon.alok@gmail.com>
2023-07-04 03:56:58 +0600

    login page design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Login></Login>
     </div>
   );
 }
